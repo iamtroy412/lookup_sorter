c4ed4048600122dc1ca3df05a39054f5248841b2
Troy Caro <iamtroy412@gmail.com>
2023-03-06 15:17:01 -0500

    add clap cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+/// A program for making DNS queries on a list of names, then trying to determine if they are on the F5
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// File with list of names to query
+    #[arg(short, long)]
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{}", &args.file);
 }
